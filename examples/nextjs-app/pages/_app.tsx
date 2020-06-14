ca72fef1444d2dbd171807680515738601d9938b
Kristopher Wuollett <klw@google.com>
2020-06-13 21:55:31 -0400

    Attempt to use React Keycloak SSR
    * Updated config variables to use NEXT_PUBLIC_ prefix

--- examples/nextjs-app/pages/_app.tsx
+++ examples/nextjs-app/pages/_app.tsx
@@ -3,13 +3,19 @@ import * as React from 'react'
 import type { IncomingMessage } from 'http'
 import type { AppProps, AppContext } from 'next/app'
 
-import { SSRKeycloakProvider, Persistors } from '@react-keycloak/nextjs'
-import type { KeycloakCookies } from  '@react-keycloak/nextjs'
+// Module '"../node_modules/@react-keycloak/nextjs"' has no exported member 'ServerPersistors'.
+// import { SSRKeycloakProvider, ServerPersistors } from '@react-keycloak/nextjs'
+
+// Module '"../node_modules/@react-keycloak/ssr"' has no exported member 'ServerPersistors'.
+// import { SSRKeycloakProvider, ServerPersistors } from '@react-keycloak/ssr'
+
+import { SSRKeycloakProvider, Persistors } from '@react-keycloak/ssr'
+import type { KeycloakCookies } from '@react-keycloak/nextjs'
 
 const keycloakCfg = {
-  realm: '',
-  url: '',
-  clientId: ''
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM ?? '',
+  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL ?? '',
+  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID ?? '',
 }
 
 interface InitialProps {
@@ -20,7 +26,7 @@ function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
   return (
     <SSRKeycloakProvider
       keycloakConfig={keycloakCfg}
-      persistor={Persistors.Cookies(cookies)}
+      persistor={Persistors.SSRCookies(cookies)}
     >
       <Component {...pageProps} />
     </SSRKeycloakProvider>
@@ -37,7 +43,7 @@ function parseCookies(req?: IncomingMessage) {
 MyApp.getInitialProps = async (context: AppContext) => {
   // Extract cookies from AppContext
   return {
-    cookies: parseCookies(context?.ctx?.req)
+    cookies: parseCookies(context?.ctx?.req),
   }
 }
 
